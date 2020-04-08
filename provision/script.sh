44c02907f80b4204cf473d592067588a01288504
Flavio Lima <flaviolima.s@live.com>
2020-04-08 19:57:12 -0300

    Correção do yum e listagem do git config após a instalação

--- provision/script.sh
+++ provision/script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sudo yum -y update
-sudo yum install -y git
+sudo yum update
+sudo yum install git
 git --version
 git config --global user.name "Flavio Lima"
 git config --global user.email "flaviolima.s@live.com"
+git config -l
