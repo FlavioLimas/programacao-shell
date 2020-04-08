b366d3a9f75ff74aaf857d7ba9cd28ef745345af
Flavio Lima <flaviolima.s@live.com>
2020-04-08 20:43:51 -0300

    Adicionado provisionamentos e configuração do git e pulll

--- provision/script.sh
+++ provision/script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-sudo yum update
-sudo yum install git
-git --version
-git config --global user.name "Flavio Lima"
-git config --global user.email "flaviolima.s@live.com"
-git config -l
+sudo echo "LANG="pt_BR.UTF-8"" > /etc/locale.conf
+sudo yum -y update
+sudo yum -y install git
+sudo git --version
+sudo git config --global user.name "Flavio Lima"
+sudo git config --global user.email "flaviolima.s@live.com"
+sudo git config -l
+cd /vagrant/
+git pull origin master
