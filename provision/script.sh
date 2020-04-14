3c474a78a741ea9df35bc723e8532718ff3658a7
Flavio Lima <flaviolima.s@live.com>
2020-04-14 15:20:25 -0300

    Comandos git

--- provision/script.sh
+++ provision/script.sh
@@ -3,7 +3,6 @@ sudo echo "LANG="pt_BR.UTF-8"" > /etc/locale.conf
 sudo apt-get -y update
 sudo apt-get -y install git
 sudo git --version
-sudo git config --global user.name "Flavio Lima"
-sudo git config --global user.email "flaviolima.s@live.com"
-sudo git remote add origin https://bitbucket.org/flaviolimas/programacao-shell
 sudo git config -l
+cd /home/vagrant/programacao-shell/
+git remote -v
