1fb9d324334f353246cc3201822366c5717adbd4
Flavio Lima <flaviolima.s@live.com>
2020-04-14 14:45:48 -0300

    Adaptação de comandos para ubuntu

--- provision/script.sh
+++ provision/script.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo echo "LANG="pt_BR.UTF-8"" > /etc/locale.conf
-sudo yum -y update
-sudo yum -y install git
+sudo apt-get -y update
+sudo apt-get -y install git
 sudo git --version
 sudo git config --global user.name "Flavio Lima"
 sudo git config --global user.email "flaviolima.s@live.com"
+sudo git remote add origin https://bitbucket.org/flaviolimas/programacao-shell
 sudo git config -l
