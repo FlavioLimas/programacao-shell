8d47b08693b849500c3ee3ee43d2531273579878
Flavio Lima <flaviolima.s@live.com>
2020-04-15 20:32:10 -0300

    Adicionado parametro -y

--- provision/script.sh
+++ provision/script.sh
@@ -10,5 +10,5 @@ sudo echo "LANG="pt_BR.UTF-8"" > /etc/locale.conf
 # Comandos para fazer o update no centos ao subir a VM no vagrant
 sudo dnf clean all
 sudo rm -r /var/cache/dnf
-sudo dnf upgrade
-sudo yum install git
+sudo dnf -y upgrade
+sudo yum -y install git
