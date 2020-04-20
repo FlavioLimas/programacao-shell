dfd09fc3adfba12c5d42c58ddac86c9ac9855790
Flavio Lima <flaviolima.s@live.com>
2020-04-20 22:12:12 +0000

    Aula de argumentos posicionais concluida

---
+++ scripts/argumentos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Um simples programa que trabalha com argumentos
+
+echo "Argumento $1"
+echo "Argumento $2"
+
+#Numeros de argumentos passados
+
+echo "Foram passados um total de $# argumentos"
+
+#Lista de todos os argumentos
+
+echo "A lista de argumentos a seguir $@"
