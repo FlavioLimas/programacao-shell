f7faa3278001efacc8cf55fd9ce4e1e5cfebc5d1
Flavio Lima <flaviolima.s@live.com>
2020-05-08 04:52:42 +0000

    Exercicio 6 jogo concluido

--- scripts/exercicios/secreto.sh
+++ scripts/exercicios/secreto.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-#Exercicio 6
-
+#Exercicio 6 usuario descobrir qual o numero secreto de 1 a 10.
 continuaExec(){
-	if [[ ${EXEC == "n"} ]]
+	if [[ ${EXEC} != "s" ]]
 	then
-		echo "Obrigado por participar"
+		echo "Obrigado por participar."
 		exit 0
 	fi
 }
-
-verificaRange(){
-	if [[ $((($RANDOM %10) + 1)) -eq ${N} ]]
+informe(){
+	echo "Caro jogador informe um numero de 1 a 10."
+	read N
+	if [[ ${N} -gt 10 ]] || [[ ${N} -lt 1 ]]
 	then
-		echo "Parabens caro jogador acertou"
-	else
-		echo "
+		echo "Favor informar um numero valido!"
+	fi
 }
-echo "Caro usuario deseja jogar? s/n"
+echo "Caro usuario deseja jogar? s/n."
 read EXEC
 continuaExec $EXEC
-
-echo "Caro jogador informe um numero de 1 a 10"
-read N
-verificaRange $N
+informe
+while [[ $((($RANDOM %10) + 1)) -ne ${N} ]]
+do
+	echo "Desculpe caro jogador errou, tente outra vez."
+	informe
+done
+echo "Parabens caro jogador acertou."
